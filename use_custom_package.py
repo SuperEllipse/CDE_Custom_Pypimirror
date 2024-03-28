cce4d8b00cc131acaf192aa9b7c0c081ba23ec38
superellipse <vishrajagopalan@cloudera.com>
2024-03-28 11:37:03 +0530

    first commit

---
+++ use_custom_package.py
@@ -0,0 +1,8 @@
+from example_package_superellipse import example
+import sys
+#import os
+# print("simple job for testing custom package")
+# print(example.add_one(2))
+print(example.add_two(3))
+print("Python version: ", sys.version)
+
